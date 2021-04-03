691436d935564dc2300d5ecf0763ac38c4ac9af5
Bhavika2006 <70019267+Bhavika2006@users.noreply.github.com>
2021-04-03 23:52:53 +0530

    Add files via upload

--- drop.js
+++ drop.js
@@ -1,20 +1,25 @@
 class Drop{
     constructor(x,y,radius){
-        options ={
-           'friciton':0.1
+        var options ={
+           'friciton':0.2
         }
-        this.body = Bodies(x,y,radius,options)
+        this.body = Bodies.circle(x,y,radius,options)
         this.radius = radius;
-        this.drop = []
         World.add(world,this.body);
-
-
     }
     display(){
+        fill("blue")
         circle(this.body.position.x,this.body.position.y,5)
        
     }
 
+    update(){
+        if(this.body.position.y>400){
+           Matter.Body.setPosition(this.body,{x: random(0,400),
+           y: random(-10,0)
+           });
+         }
+      }
    
     }
    
--- sketch.js
+++ sketch.js
@@ -3,20 +3,24 @@ const Body = Matter.Body
 const Bodies = Matter.Bodies
 const World = Matter.World
 var manImg,thunderboltImg
+var man, drop
 drops = []
+var maxDrops =100
+var thunder, thunderCreatedFrame = 0;
+
 function preload(){
-    manImg = loadImage("image/man/walking_1.png")  
-    manImg = loadImage("image/man/walking_2.png")  
-    manImg = loadImage("image/man/walking_3.png")  
-    manImg = loadImage("image/man/walking_4.png")  
-    manImg = loadImage("image/man/walking_5.png")  
-    manImg = loadImage("image/man/walking_6.png")  
-    manImg = loadImage("image/man/walking_7.png")  
-    manImg = loadImage("image/man/walking_8.png")  
-    thunderboltImg = loadImage("image/thunderbolt/1.png")
-    thunderboltImg = loadImage("image/thunderbolt/2.png")
-    thunderboltImg = loadImage("image/thunderbolt/3.png")
-    thunderboltImg = loadImage("image/thunderbolt/4.png")
+  manWalking= loadAnimation("images/man/walking_1.png"
+  ,"images/man/walking_2.png","images/man/walking_3.png" 
+  ,"images/man/walking_4.png"  
+  ,"images/man/walking_5.png"  
+  ,"images/man/walking_6.png"  
+  ,"images/man/walking_7.png"  
+  ,"images/man/walking_8.png")  
+  thunder1 = loadImage("images/thunderbolt/1.png")
+  thunder2 = loadImage("images/thunderbolt/2.png")
+  thunder3 = loadImage("images/thunderbolt/3.png")
+  thunder4 = loadImage("images/thunderbolt/4.png")
+    
 }
 
 function setup(){
@@ -24,18 +28,50 @@ function setup(){
     engine = Engine.create()
     world = engine.world
     
-    man = new Man(200,200,20)
+    man = new Man(200,200,60)
+
+    if(frameCount%100===0){
+        for(var i=0; i<maxDrops;i++){
+          drops.push(new Drop(random(0,400),random(0,400),3,10));
+         }
+        }
+
+    manImage = createSprite(man.body.position.x,man.body.position.y+35)
+    manImage.addAnimation("walking",manWalking)
+    manImage.scale = 0.3
 
 }
 
 function draw(){
+    background("black");
   Engine.update(engine);
-   var maxDrops =100
-   for(i = 0;i<maxDrops;i++){
-       drop = new Drop(random(0,400),random(0,400))
-       drops.push(drop)
+
+  var rand = Math.round(random(1,4));
+  if(frameCount%80===0){
+    thunderCreatedFrame = frameCount;
+   thunder = createSprite(random(10,370),random(10,30),10,10);
+   thunder.scale = 0.3;
+   switch(rand){
+     case 1 : thunder.addImage(thunder1);
+     break;
+     case 2 : thunder.addImage(thunder2);
+     break;
+     case 3 : thunder.addImage(thunder3);
+     break;
+     case 3 : thunder.addImage(thunder4);
+     break;
+     default : break;
    }
-    man.display();
-    drop.display();
+}
+
+if(thunderCreatedFrame + 20 === frameCount && thunder){
+    thunder.destroy();
+ }
+
+   for(var i=0; i<maxDrops;i++){
+    drops[i].display();
+    drops[i].update();
+  }
+    drawSprites();
 }   
 
